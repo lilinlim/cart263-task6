f833d90d7ec6b443b4b45a238d697f56b6ee7677
lilin <118693994+lilinlim@users.noreply.github.com>
2026-03-26 10:51:35 -0400

    add/remove circular obj

--- js/DrawingBoard.js
+++ js/DrawingBoard.js
@@ -1,6 +1,6 @@
 class DrawingBoard {
   /* Constructor */
-  constructor(canvas, context,drawingBoardId) {
+  constructor(canvas, context, drawingBoardId) {
     this.canvas = canvas;
     this.context = context;
     this.objectsOnCanvas = [];
@@ -24,44 +24,73 @@ class DrawingBoard {
     console.log(this.mouseOffsetX, this.mouseOffsetY);
     //differentiate which canvas
     //you can remove the console.logs /// 
-    if(this.drawingBoardId ==="partA"){
-      console.log("in A")
+    if (this.drawingBoardId === "partA") {
+      console.log("in A");
+
     }
-    if(this.drawingBoardId ==="partB"){
+    if (this.drawingBoardId === "partB") {
       console.log("in B")
     }
-    if(this.drawingBoardId ==="partC"){
+    if (this.drawingBoardId === "partC") {
       console.log("in C")
     }
-    if(this.drawingBoardId ==="partD"){
+    if (this.drawingBoardId === "partD") {
       console.log("in D")
       for (let i = 0; i < this.objectsOnCanvas.length; i++) {
         if (typeof this.objectsOnCanvas[i].updatePositionRect === "function") {
           this.objectsOnCanvas[i].updatePositionRect(this.mouseOffsetX, this.mouseOffsetY);
         }
       }
-   }
+    }
   }
 
   clickCanvas(e) {
-   // console.log("clicked");
+    // console.log("clicked");
     this.canvasBoundingRegion = this.canvas.getBoundingClientRect();
     this.mouseOffsetX = parseInt(e.clientX - this.canvasBoundingRegion.x);
     this.mouseOffsetY = parseInt(e.clientY - this.canvasBoundingRegion.y);
     //console.log(this.mouseOffsetX, this.mouseOffsetY);
-     
+
     //differentiate which canvas
-   //you can remove the console.logs /// 
-     if(this.drawingBoardId ==="partA"){
+    //you can remove the console.logs /// 
+    if (this.drawingBoardId === "partA") {
       console.log("in A")
+
+      for (let i = this.objectsOnCanvas.length - 1; i >= 0; i--) {
+        let circ = this.objectsOnCanvas[i];
+
+        let dx = this.mouseOffsetX - circ.x;
+        let dy = this.mouseOffsetY - circ.y;
+
+        if (dx * dx + dy * dy <= circ.radius * circ.radius) {
+          this.objectsOnCanvas.splice(i, 1);
+          this.display();
+          return;
+        }
+      }
+
+      let colorArray = [
+        "lightpink",
+        "mistyrose",
+        "palevioletred",
+        "pink",
+        "thistle",
+      ];
+      let randomColor = colorArray[parseInt(Math.random() * colorArray.length)];
+
+      let newCircObj = new CircularObj(this.mouseOffsetX + Math.random() * 20, this.mouseOffsetY + Math.random() * 20, 20, randomColor, "#E6E6FA", this.context)
+      this.addObj(newCircObj);
+      this.display();
+      //console.log(this.objectsOnCanvas.length);
+
     }
-    if(this.drawingBoardId ==="partB"){
+    if (this.drawingBoardId === "partB") {
       console.log("in B")
     }
-    if(this.drawingBoardId ==="partC"){
+    if (this.drawingBoardId === "partC") {
       console.log("in C")
     }
-    if(this.drawingBoardId ==="partD"){
+    if (this.drawingBoardId === "partD") {
       console.log("in D")
       for (let i = 0; i < this.objectsOnCanvas.length; i++) {
         if (typeof this.objectsOnCanvas[i].changeColor === "function") {
@@ -69,7 +98,7 @@ class DrawingBoard {
           this.objectsOnCanvas[i].changeColor(randomColor);
         }
       }
-      }
+    }
   }
   /* method to add obj to canvas */
   addObj(objToAdd) {
@@ -85,14 +114,15 @@ class DrawingBoard {
 
   /* method to add animate objects on canvas */
   animate() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
     for (let i = 0; i < this.objectsOnCanvas.length; i++) {
-     this.context.clearRect(0,0,this.canvas.width,this.canvas.height)
-     this.objectsOnCanvas[i].update();
-     this.objectsOnCanvas[i].display();
+      this.objectsOnCanvas[i].update();
+      this.objectsOnCanvas[i].display();
     }
   }
 
-  run(videoElement){
+  run(videoElement) {
     for (let i = 0; i < this.objectsOnCanvas.length; i++) {
       this.objectsOnCanvas[i].update(videoElement);
       this.objectsOnCanvas[i].display();
